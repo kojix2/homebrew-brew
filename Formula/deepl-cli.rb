56e29fcd40b79ab4df0fceeb6186cb70a0a9695b
kojix2 <2xijok@gmail.com>
2024-03-22 18:37:44 +0900

    Add desc

--- Formula/deepl-cli.rb
+++ Formula/deepl-cli.rb
@@ -1,15 +1,15 @@
 class DeeplCli < Formula
-  version 'v0.2.5'
-  desc ''
-  homepage ''
-  url 'https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.2.5.tar.gz'
-  sha256 '3ea63f615eecc5234c8d449be7fb41f9f6ebe72f36b8d231ed9756b7a22386ef'
-  license 'MIT'
+  version "v0.2.5"
+  desc "Simple command line tool for DeepL"
+  homepage "https://github.com/kojix2/deepl-cli"
+  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "3ea63f615eecc5234c8d449be7fb41f9f6ebe72f36b8d231ed9756b7a22386ef"
+  license "MIT"
 
-  depends_on 'crystal'
+  depends_on "crystal"
 
   def install
-    system 'shards build --release'
-    bin.install 'bin/deepl'
+    system "shards build --release"
+    bin.install "bin/deepl"
   end
 end
