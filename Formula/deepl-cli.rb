a26e681b86e86dd096ceb3f813672a665356b4ce
kojix2 <2xijok@gmail.com>
2024-03-27 15:18:17 +0900

    Update deepl-cli.rb

--- Formula/deepl-cli.rb
+++ Formula/deepl-cli.rb
@@ -2,8 +2,8 @@ class DeeplCli < Formula
   version "v0.2.6"
   desc "Simple command line tool for DeepL"
   homepage "https://github.com/kojix2/deepl-cli"
-  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.2.6.tar.gz"
-  sha256 "0dae7fd78113123b57105c3cfa5e07f182dfa934b7034eb16ebeb3856271d4a2"
+  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.3.0.beta2.tar.gz"
+  sha256 "cd365150efd1e5eafd32c12653dde6f79e6a54f5f7d2b6a0bea6b93f90542f7f"
   license "MIT"
 
   depends_on "crystal"
