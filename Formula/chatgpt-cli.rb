77603c94ca542d8036c29e9dca2015b9a57daa41
kojix2 <2xijok@gmail.com>
2024-04-02 13:01:51 +0900

    Use git url instead of tarball

--- Formula/chatgpt-cli.rb
+++ Formula/chatgpt-cli.rb
@@ -2,8 +2,9 @@ class ChatgptCli < Formula
   version "v0.4.3"
   desc "Simple command line tool for ChatGPT"
   homepage "https://github.com/kojix2/chatgpt-cli"
-  url "https://github.com/kojix2/chatgpt-cli/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "4eb0375b2407ec2ae01e15e260f526a052a1f89e57fedba0b419f3118b917489"
+  url "https://github.com/kojix2/chatgpt-cli.git",
+      tag: "v0.4.3",
+      revision: "e6bd7635e75322c217388c46aec2b1073869cac0"
   license "MIT"
 
   depends_on "crystal"
