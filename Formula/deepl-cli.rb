4ff3fa84e716dfa296aedb3a64018ee9d9235610
kojix2 <2xijok@gmail.com>
2024-07-17 09:42:29 +0900

    Set deepl-cli version to 0.4.0

--- Formula/deepl-cli.rb
+++ Formula/deepl-cli.rb
@@ -1,12 +1,13 @@
 class DeeplCli < Formula
-  version "v0.3.5"
+  version "v0.4.0"
   desc "Simple command line tool for DeepL"
   homepage "https://github.com/kojix2/deepl-cli"
-  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "1ead37f41549019b54bdf7130576001ca1a49eb0c1c6d4e53bc4f8bb39909fc1"
+  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "483d30341a80914dfe231a58819c9adc5c8f6d867b71eb45e9c39b6950bc8380"
   license "MIT"
 
   depends_on "crystal"
+  depends_on "readline"
 
   def install
     system "shards build --release"
