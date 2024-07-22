8c1346f276a09fb9bb4a4c6965e9765fa9b94c93
kojix2 <2xijok@gmail.com>
2024-07-22 16:53:25 +0900

    Set checksum.cr version to v0.1.3

--- Formula/checksum.rb
+++ Formula/checksum.rb
@@ -1,9 +1,9 @@
 class Checksum < Formula
-  version "v0.1.2"
+  version "v0.1.3"
   desc "A more comfortable checksum"
   homepage "https://github.com/kojix2/checksum.cr"
-  url "https://github.com/kojix2/checksum.cr/archive/tags/v0.1.2.tar.gz"
-  sha256 "9d003504339bd291b6fa49b0bd05c9642715d9aff0ba598cc235b70ef5195d42"
+  url "https://github.com/kojix2/checksum.cr/archive/tags/v0.1.3.tar.gz"
+  sha256 "670f27559b93b55a385ea5e58e547dae7e5d8504e5c8b5274b3f32e1d527fad0"
   license "MIT"
 
   depends_on "crystal"
