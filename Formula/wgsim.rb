a0274ded8f2dbc97267eb4c8b4169016200f29f7
kojix2 <2xijok@gmail.com>
2024-09-24 00:02:26 +0900

    Update wgsim

--- Formula/wgsim.rb
+++ Formula/wgsim.rb
@@ -1,16 +1,16 @@
 class Wgsim < Formula
-  version "v0.0.2.alpha"
+  version "v0.0.7.alpha"
   desc "Reimplement wgsim in Crystal and add extra features."
   homepage "https://github.com/kojix2/wgsim"
-  url "https://github.com/kojix2/wgsim.cr/archive/refs/tags/v0.0.2.alpha.tar.gz"
-  sha256 "f85601b276700d8b3c3d5208b2cb53e4f486338ad119874e411d13a8ee09381e"
+  url "https://github.com/kojix2/wgsim.cr/archive/refs/tags/v0.0.7.alpha.tar.gz"
+  sha256 "5def687443fb46490db096f42f86ca78b9830fff3cb9e9363989a59700b769b1"
   license "MIT"
 
   depends_on "crystal"
 
   def install
     system "shards build --release"
-    # system "shards build --release -Dpreview_mt"
+    # system "shards build --release -Dpreview_m"
     bin.install "bin/wgsim"
   end
 end
