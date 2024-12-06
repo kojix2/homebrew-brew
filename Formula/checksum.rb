7333c7d68a80cf95f8f7e31963441b9ddee2b907
kojix2 <2xijok@gmail.com>
2024-12-06 14:43:51 +0900

    Update checksum.rb

--- Formula/checksum.rb
+++ Formula/checksum.rb
@@ -3,7 +3,7 @@ class Checksum < Formula
   desc "A more comfortable checksum"
   homepage "https://github.com/kojix2/checksum.cr"
   url "https://github.com/kojix2/checksum.cr/archive/tags/v0.1.5.tar.gz"
-  sha256 "efaac35697e5f62bc978d4b27aba1eb5a92a6aaf0070d65e3f3f77245e22ec6c"
+  sha256 "85bd50d541b4d9944c1e18f8d43855696f3d299a1f791477a9d043924d2517ae"
   license "MIT"
 
   depends_on "crystal"
