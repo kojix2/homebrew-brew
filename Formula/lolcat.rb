7ce32a6cd0b4c8bacf7c794ce197e08d0082b950
kojix2 <2xijok@gmail.com>
2025-08-01 11:15:24 +0900

    Set lolcat.cr version to v0.0.8

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -1,9 +1,9 @@
 class Lolcat < Formula
-  version "v0.0.6"
+  version "v0.0.8"
   desc "Crystal implementation of the lolcat "
   homepage "https://github.com/kojix2/lolcat.cr"
-  url "https://github.com/kojix2/lolcat.cr/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "30db64c581f51478a1da8a8cdca3d339ca5be27f46c8277f168eb7042b1cd962"
+  url "https://github.com/kojix2/lolcat.cr/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "b4cd8c87a74d1a85af75672ea6c2ca1eba777f2230f9348b5d5147fd74708cf2"
   license "BSD-3-Clause"
 
   depends_on "crystal"
