44e43ed7554554a9a18195c2fe2d11affdda1412
kojix2 <2xijok@gmail.com>
2025-12-28 17:07:07 +0900

    Update lolcat to version 0.0.10

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -1,9 +1,9 @@
 class Lolcat < Formula
-  version "v0.0.8"
+  version "v0.0.10"
   desc "Crystal implementation of the lolcat "
   homepage "https://github.com/kojix2/lolcat.cr"
-  url "https://github.com/kojix2/lolcat.cr/archive/refs/tags/v0.0.8.tar.gz"
-  sha256 "b4cd8c87a74d1a85af75672ea6c2ca1eba777f2230f9348b5d5147fd74708cf2"
+  url "https://github.com/kojix2/lolcat.cr/archive/refs/tags/v0.0.10.tar.gz"
+  sha256 "16589dcec451b8d1849c869adf85f1c910ea76f09b7beb1878ff218a7cbf7bb0"
   license "BSD-3-Clause"
 
   depends_on "crystal"
