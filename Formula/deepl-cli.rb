7388d13f8969ff4a0af532ddd97e453cb28fe02f
kojix2 <2xijok@gmail.com>
2026-01-22 22:36:54 +0900

    Set deepl-cli version to v0.5.0

--- Formula/deepl-cli.rb
+++ Formula/deepl-cli.rb
@@ -1,9 +1,9 @@
 class DeeplCli < Formula
-  version "v0.4.12"
+  version "v0.5.0"
   desc "Simple command line tool for DeepL"
   homepage "https://github.com/kojix2/deepl-cli"
-  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.4.12.tar.gz"
-  sha256 "d1341d8cef575bb182f18bf18850d35d59fe258ce896e6702d22ae25c82a19fa"
+  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "851d5c0272c5123071d5b3cb244e5867a247440c06cd47105559f468fd8ad6d4"
   license "MIT"
 
   depends_on "crystal"
