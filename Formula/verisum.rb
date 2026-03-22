24b4c940f24c988a1d1fc2a75a46c50e3dcb20e5
kojix2 <2xijok@gmail.com>
2026-03-22 10:48:28 +0900

    Update Verisum formula to include build dependency

--- Formula/verisum.rb
+++ Formula/verisum.rb
@@ -1,15 +1,18 @@
 class Verisum < Formula
-  version "v0.1.8"
   desc "A more comfortable checksum"
   homepage "https://github.com/kojix2/verisum"
   url "https://github.com/kojix2/verisum/archive/tags/v0.1.8.tar.gz"
   sha256 "f4f2ff7bf98dace7efceeb2dbdee8560ad3459677bcf1b30300aab07028125cb"
   license "MIT"
 
-  depends_on "crystal"
+  depends_on "crystal" => :build
 
   def install
     system "shards build --release"
     bin.install "bin/verisum"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/verisum --version")
   end
 end
