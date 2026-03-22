054895e89f6e9d4e96078271c5050f385784294e
kojix2 <2xijok@gmail.com>
2026-03-22 10:41:28 +0900

    Update ChatGPT CLI version to v0.7.0

--- Formula/chatgpt-cli.rb
+++ Formula/chatgpt-cli.rb
@@ -1,5 +1,4 @@
 class ChatgptCli < Formula
-  version "v0.6.1"
   desc "Simple command line tool for ChatGPT"
   homepage "https://github.com/kojix2/chatgpt-cli"
   url "https://github.com/kojix2/chatgpt-cli.git",
@@ -7,12 +6,16 @@ class ChatgptCli < Formula
       revision: "8884baebbefceb9d0dd1cbc70cb6460cb577b9cc"
   license "MIT"
 
-  depends_on "crystal"
+  depends_on "crystal" => :build
   depends_on "readline"
   depends_on "bat"
 
   def install
     system "make build release=1"
     bin.install "bin/chatgpt"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/chatgpt --version")
   end
 end
