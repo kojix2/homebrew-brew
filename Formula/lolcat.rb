c16705ca7e52fd9ae78efec20a848a33a147019d
kojix2 <2xijok@gmail.com>
2026-03-22 10:43:13 +0900

    Add test block for version verification
    Add a test block to verify the version of lolcat.

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -1,15 +1,18 @@
 class Lolcat < Formula
-  version "v0.0.10"
   desc "Crystal implementation of the lolcat "
   homepage "https://github.com/kojix2/lolcat.cr"
   url "https://github.com/kojix2/lolcat.cr/archive/refs/tags/v0.0.10.tar.gz"
   sha256 "16589dcec451b8d1849c869adf85f1c910ea76f09b7beb1878ff218a7cbf7bb0"
   license "BSD-3-Clause"
 
-  depends_on "crystal"
+  depends_on "crystal" => :build
 
   def install
     system "shards build --release"
     bin.install "bin/lolcat"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lolcat --version")
   end
 end
