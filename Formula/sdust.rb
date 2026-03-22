3775bd51b57716b9e7808006cd7705c90c685408
kojix2 <2xijok@gmail.com>
2026-03-22 10:45:48 +0900

    Update sdust.rb with new homepage and dependency
    Updated homepage URL and modified dependency declaration.

--- Formula/sdust.rb
+++ Formula/sdust.rb
@@ -1,12 +1,11 @@
 class Sdust < Formula
-  version "v0.1.4.alpha"
   desc "Reimplement sdust in Crystal"
-  homepage "https://github.com/kojix2/sdust"
+  homepage "https://github.com/kojix2/sdust.cr"
   url "https://github.com/kojix2/sdust.cr/archive/refs/tags/v0.1.4.alpha.tar.gz"
   sha256 "de64cb0edf8608a365c52d6230ed7d1906b9fff270880e01c47a7e84ec4c85da"
   license "MIT"
 
-  depends_on "crystal"
+  depends_on "crystal" => :build
 
   def install
     system "shards build --release -Dpreview_mt"
