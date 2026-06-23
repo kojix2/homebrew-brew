76affc82612dd6a80c8d51c10e1a7a23c67342cb
kojix2 <2xijok@gmail.com>
2026-06-23 11:03:06 +0900

    Set deepl-cli version to v0.6.0

--- Formula/deepl-cli.rb
+++ Formula/deepl-cli.rb
@@ -1,13 +1,14 @@
 class DeeplCli < Formula
   desc "Simple command line tool for DeepL"
   homepage "https://github.com/kojix2/deepl-cli"
-  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.5.4.tar.gz"
-  sha256 "abfc0268a34cb5e8e24ee2986b7b608336b4ae0e34b1818c4d85679d181b66d6"
+  url "https://github.com/kojix2/deepl-cli/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "6e891ad83cf24abb6cd977118f86379fd1af9aebc5c892c7fcef8f9ac79d6f4f"
   license "MIT"
 
   depends_on "crystal" => :build
   depends_on "openssl@3"
-
+  depends_on "pcre2"
+  
   def install
     system "shards", "build", "--release"
     bin.install "bin/deepl"
