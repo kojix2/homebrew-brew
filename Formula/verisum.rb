064fb563c7fb7ac761933c73d4770756381629fb
kojix2 <2xijok@gmail.com>
2026-06-23 14:51:55 +0900

    Set verisum version to v0.2.0

--- Formula/verisum.rb
+++ Formula/verisum.rb
@@ -1,12 +1,13 @@
 class Verisum < Formula
   desc "A more comfortable checksum"
   homepage "https://github.com/kojix2/verisum"
-  url "https://github.com/kojix2/verisum/archive/tags/v0.1.8.tar.gz"
-  sha256 "f4f2ff7bf98dace7efceeb2dbdee8560ad3459677bcf1b30300aab07028125cb"
+  url "https://github.com/kojix2/verisum/archive/tags/v0.2.0.tar.gz"
+  sha256 "e8e1bb5cb5167f651f4eb447e1781c925e4b64efbfe3e88e866843a0eca3fada"
   license "MIT"
 
   depends_on "crystal" => :build
   depends_on "openssl@3"
+  depends_on "pcre2"
 
   def install
     system "shards build --release"
